b0b56368660eb27bcf6e9b43603906da988f0ab8
Chris Vaughan <vaughandroid@gmail.com>
2024-01-26 11:25:01 +0000

    Add some TODOs

--- sample/project/.phoenixrc.ts
+++ sample/project/.phoenixrc.ts
@@ -8,19 +8,23 @@ project.tokens = {
   projectName: 'sample-project',
   projectVersion: '0.0.2',
   projectDescription: 'A sample project',
+  // TODO: Do this automatically?
   generatedFileComment: GENERATED_FILE_COMMENT,
 }
 
 const packageJson = new JsonProjectFile({
+  // TODO: Set template path for project
   fileName: 'package.json',
   templatePath: `${__dirname}/../template/package.json`,
   regenerate: true,
+  // TODO: Json types not actually useful?
+  customiseJson: (json: any) => {
+    // TODO: Add phoenix dependency automatically?
+    // Install from the local filesystem rather than NPM.
+    json['devDependencies']['@vaughandroid/phoenix'] = 'link:../../'
+    return json
+  }
 })
-packageJson.customiseJson = (json: any) => {
-  // Install from the local filesystem rather than NPM.
-  json['devDependencies']['@vaughandroid/phoenix'] = 'link:../../'
-  return json
-}
 
 project.projectFiles = [packageJson]
 
