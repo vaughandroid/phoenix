234221a16def7c13bac993a45db86ab395c6e12a
Chris Vaughan <vaughandroid@gmail.com>
2024-01-29 13:35:38 +0000

    Flesh out sample project & template

--- sample/project/.phoenixrc.ts
+++ sample/project/.phoenixrc.ts
@@ -1,5 +1,5 @@
 // Would be from "@vaughandroid/phoenix" in a real project
-import { Json, JsonProjectFile, Project } from '../../src'
+import { Json, JsonProjectFile, Project, ProjectFile } from '../../src'
 import { GENERATED_FILE_COMMENT } from '../../src/projects/project'
 
 const project = new Project()
@@ -12,10 +12,12 @@ project.tokens = {
   generatedFileComment: GENERATED_FILE_COMMENT,
 }
 
+// TODO: Add 'from template' constructor which defaults the filename
 const packageJson = new JsonProjectFile({
   // TODO: Set template path for project
   fileName: 'package.json',
   templatePath: `${__dirname}/../template/package.json`,
+  // TODO: Default regenerate to true?
   regenerate: true,
   // TODO: Json types not actually useful?
   customiseJson: (json: any) => {
@@ -23,9 +25,34 @@ const packageJson = new JsonProjectFile({
     // Install from the local filesystem rather than NPM.
     json['devDependencies']['@vaughandroid/phoenix'] = 'link:../../'
     return json
-  }
+  },
 })
 
-project.projectFiles = [packageJson]
+
+const eslintrcJs = new ProjectFile({
+  fileName: '.eslintrc.js',
+  templatePath: `${__dirname}/../template/.eslintrc.js`,
+  regenerate: true,
+})
+
+const gitignore = new ProjectFile({
+  fileName: '.gitignore',
+  templatePath: `${__dirname}/../template/.gitignore`,
+  regenerate: true,
+})
+
+const prettierrcJs = new ProjectFile({
+  fileName: '.prettierrc.js',
+  templatePath: `${__dirname}/../template/.prettierrc.js`,
+  regenerate: true,
+})
+
+const tsconfigJson = new JsonProjectFile({
+  fileName: 'tsconfig.json',
+  templatePath: `${__dirname}/../template/tsconfig.json`,
+  regenerate: true,
+})
+
+project.projectFiles = [packageJson, eslintrcJs, gitignore, prettierrcJs, tsconfigJson]
 
 project.regenerateFiles()
---
+++ sample/template/.prettierrc.js
@@ -0,0 +1,5 @@
+// {{generatedFileComment}}
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2
+}
